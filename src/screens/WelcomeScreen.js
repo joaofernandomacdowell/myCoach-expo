ff635181e5d5bd946ce8004233d185c805bbcf36
João Fernando <joaofernando.costa@corp.globo.com>
2017-09-15 23:57:01 -0300

    Create screens and start to apply react-navigation

---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import Welcome from '../components/presentation/Welcome';
+
+
+class WelcomeScreen extends Component {
+  render() {
+    return (
+      <Welcome />
+    );
+  }
+}
+
+export default WelcomeScreen;
