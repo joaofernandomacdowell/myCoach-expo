26595dd8b30dc0818f887d27ae1b590e3dff48cb
João Fernando <joaofernando.costa@corp.globo.com>
2017-09-17 22:36:33 -0300

    Refactor 'Welcome' component and 'WelcomeScreen'

--- src/components/presentation/Welcome.js
+++ src/components/presentation/Welcome.js
@@ -1,55 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
 import {
   mainViewStyles,
   imageStyles,
   loginButtonStyles,
   startButtonStyles
-} from '../../styles/presentation';
+} from '../../styles/welcome';
 
 import { CardSection } from '../common';
 import WelcomeText from './WelcomeText';
 import ButtonSection from './ButtonSection';
 
 
-const Welcome = () => {
-  const { image } = imageStyles;
-  const { container } = mainViewStyles;
-  const { loginButton, loginText } = loginButtonStyles;
-  const { startButton, startText } = startButtonStyles;
+class Welcome extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <View style={container}>
-      <View>
-        <Image
-          style={image}
-          source={require('../../../resources/img/mycoach-logo.png')}
-          resizeMode="contain"
-        />
+  render() {
+    const { image } = imageStyles;
+    const { container } = mainViewStyles;
+    const { loginButton, loginText } = loginButtonStyles;
+    const { startButton, startText } = startButtonStyles;
+
+    return (
+      <View style={container}>
+        <View>
+          <Image
+            style={image}
+            source={require('../../../resources/img/mycoach-logo.png')}
+            resizeMode="contain"
+          />
+        </View>
+
+        <WelcomeText>
+          myCoach helps you achieve a healthy lifestyle!
+        </WelcomeText>
+
+        <ButtonSection>
+          <TouchableOpacity
+            onPress={this.props.onStartnow}
+            style={startButton}
+          >
+            <Text style={startText}>
+              START NOW
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={this.props.onLogin}
+            style={loginButton}
+          >
+            <Text style={loginText}>
+              LOG IN
+            </Text>
+          </TouchableOpacity>
+        </ButtonSection>
       </View>
-
-      <WelcomeText>
-        myCoach helps you achieve a healthy lifestyle!
-      </WelcomeText>
-
-      <ButtonSection>
-        <TouchableOpacity
-          onPress={() => this.props.navigation.navigate('questionSection')}
-          style={startButton}
-        >
-          <Text style={startText}>
-            START NOW
-          </Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={loginButton}>
-          <Text style={loginText}>
-            LOG IN
-          </Text>
-        </TouchableOpacity>
-      </ButtonSection>
-    </View>
-  );
-};
+    );
+  }
+}
 
 export default Welcome;
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
 import { ScreenStyles } from '../styles/constants';
-import {
-  mainViewStyles,
-  imageStyles,
-  loginButtonStyles,
-  startButtonStyles
-} from '../styles/presentation';
 
-import WelcomeText from '../components/presentation/WelcomeText';
-import ButtonSection from '../components/presentation/ButtonSection';
+import Welcome from '../components/presentation/Welcome';
 
 
 class WelcomeScreen extends Component {
@@ -17,43 +10,14 @@ class WelcomeScreen extends Component {
     super(props);
   }
 
+  onStartnowPress = () => {
+    this.props.navigation.navigate('question');
+  }
+
   render() {
-    const { image } = imageStyles;
-    const { container } = mainViewStyles;
-    const { loginButton, loginText } = loginButtonStyles;
-    const { startButton, startText } = startButtonStyles;
-    const { navigate } = this.props.navigation;
-
     return (
-      <View style={[ScreenStyles, container]}>
-        <View>
-          <Image
-            style={image}
-            source={require('../../resources/img/mycoach-logo.png')}
-            resizeMode="contain"
-          />
-        </View>
-
-        <WelcomeText>
-          myCoach helps you achieve a healthy lifestyle!
-        </WelcomeText>
-
-        <ButtonSection>
-          <TouchableOpacity
-            onPress={() => navigate('question')}
-            style={startButton}
-          >
-            <Text style={startText}>
-              START NOW
-            </Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={loginButton}>
-            <Text style={loginText}>
-              LOG IN
-            </Text>
-          </TouchableOpacity>
-        </ButtonSection>
+      <View style={ScreenStyles}>
+        <Welcome onStartnow={this.onStartnowPress} />
       </View>
     );
   }
