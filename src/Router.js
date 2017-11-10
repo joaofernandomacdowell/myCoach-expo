52ac8b37dd43ed2b4d0277eb346695f1d123640d
João Fernando <joaofernando.costa@corp.globo.com>
2017-11-10 16:46:50 -0200

    Configure Router.js to work with react-native-router-flux

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,20 @@
-import { StackNavigator } from 'react-navigation';
-
+import React from 'react';
+import { Scene, Router, Stack } from 'react-native-router-flux';
 import RegisterScreen from './screens/RegisterScreen';
 import QuestionScreen from './screens/QuestionScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
-import HomeScreen from './screens/HomeScreen';
 
-const Router = StackNavigator({
-  welcome: { screen: WelcomeScreen },
-  question: { screen: QuestionScreen },
-  register: { screen: RegisterScreen },
-  home: { screen: HomeScreen }
-}, { headerMode: 'none', initialRouteName: 'question' });
 
-export default Router;
+const RouterComponent = () => {
+  return (
+    <Router sceneStyle={{ flex: 1, backgroundColor: '#153041' }}>
+      <Stack key="root">
+        <Scene initial hideNavBar key="welcomeScreen" component={WelcomeScreen} />
+        <Scene hideNavBar key="questionScreen" component={QuestionScreen} />
+        <Scene hideNavBar key="registerScreen" component={RegisterScreen} />
+      </Stack>
+    </Router>
+  );
+};
+
+export default RouterComponent;
