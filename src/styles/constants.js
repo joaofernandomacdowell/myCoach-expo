41104f16139e33e3704a7423608ea8077e98ead0
João Fernando <jfmcdowel2@gmail.com>
2018-10-24 00:54:15 -0300

    Add more colors

--- src/styles/constants.js
+++ src/styles/constants.js
@@ -1,10 +1,14 @@
 export const Colors = {
-  darkBlue: '#153041',
+  darkBlue: '#112734',
   darkGreen: '#1d3e53',
   darkGrey: '#696670',
+  regularBlue: '#153041',
+  regularGrey: '#eeeeee',
+  lightBlue: '#265774',
   lightGreen: '#13F59D',
   lightGrey: '#F2F1F1',
-  lightRed: '#ff0033'
+  lightRed: '#ff0033',
+  white: '#FFFFFF',
 };
 
 export const ScreenStyles = {
