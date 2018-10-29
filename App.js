ecb286ad3c7dd8fb403811c9c31b1621ff7605c8
João Fernando <jfmcdowel2@gmail.com>
2018-10-29 10:52:44 -0300

    Fix error: remove wrong import

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import reducers from './src/reducers';
 import firebase from 'firebase';
 import { firebaseConfig } from './firebaseConfig';
 
